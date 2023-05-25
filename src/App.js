0e519d29b3aae2671a53cdac88c8f19794e0fa7a
EfimeroM <martinperez9713@gmail.com>
2023-05-25 16:54:53 -0300

    fix: add dependence dotenv

---
+++ src/App.js
@@ -0,0 +1,11 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const server = app.listen(PORT, () => console.log(`Server up in port: ${PORT}`))
+server.on('error', error => console.log(`Error in server ${error.message}`))
