2156548aafa94ee4929358f049e49d306aa9033d
EfimeroM <martinperez9713@gmail.com>
2023-06-09 21:06:37 -0300

    feat: add cors

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ const express = require('express')
 require('dotenv').config()
 const connectDb = require('./db/connection')
 const apiRouter = require('./routes/index.router')
+const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT || 8080
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+  origin: '*'
+}))
 
 connectDb()
 const server = app.listen(PORT, () => console.log(`Server up in port: ${PORT}`))
