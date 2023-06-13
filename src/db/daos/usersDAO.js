0f8d71b8b810f0904c8daad1ede37953ccfa1939
EfimeroM <martinperez9713@gmail.com>
2023-06-13 16:08:41 -0300

    feat: add user DAO

---
+++ src/db/daos/usersDAO.js
@@ -0,0 +1,35 @@
+const { createHash } = require('../../utils/bcrypt')
+const userSchema = require('../models/user.model')
+
+class UserDAO {
+  constructor () {
+    this.schema = userSchema
+    this.nameSchema = this.constructor.name.replace('DAO', '').toLowerCase()
+  }
+
+  async getByEmail (email) {
+    try {
+      return await this.schema.findOne({ email })
+    } catch (error) {
+      throw new Error(`Error getting ${this.nameSchema} by email - ${error.message}`)
+    }
+  }
+
+  async add (user) {
+    try {
+      const exist = await this.getByEmail(user.email)
+      if (exist) {
+        throw new Error(`User with email ${user.email} already exists`)
+      }
+      const newUser = {
+        ...user,
+        password: createHash(user.password)
+      }
+      return await this.schema.create(newUser)
+    } catch (error) {
+      throw new Error(`Error adding ${this.nameSchema} - ${error.message}`)
+    }
+  }
+}
+
+module.exports = UserDAO
