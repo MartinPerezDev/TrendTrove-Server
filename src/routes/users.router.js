81d1aa1d00a0f8f13dd60c2c332b8a50c2a98f72
EfimeroM <martinperez9713@gmail.com>
2023-06-24 21:34:53 -0300

    feat: add route post / in /api/users

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -3,9 +3,10 @@ const usersRouter = express.Router()
 const UsersController = require('../controllers/users.controller')
 
 const usersController = new UsersController()
-const { signup, login } = usersController
+const { signup, login, getUserByToken } = usersController
 
 usersRouter.post('/signup', signup)
 usersRouter.post('/login', login)
+usersRouter.post('/', getUserByToken)
 
 module.exports = usersRouter
