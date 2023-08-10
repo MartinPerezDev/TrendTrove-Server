cb9a23a0dc6fb1a90d4ca0fab1d816f4c83c6163
EfimeroM <martinperez9713@gmail.com>
2023-08-10 16:49:12 -0300

    feat: add method getByEmail in orders.controller

--- src/controllers/orders.controllers.js
+++ src/controllers/orders.controllers.js
@@ -5,6 +5,18 @@ class OrdersController extends BaseController {
   constructor () {
     super(OrdersDAO)
   }
+
+  getByEmail = async (req, res) => {
+    try {
+      const email = req.email
+      const orders = await this.dao.getByEmail(email)
+      orders.length
+        ? this.handleResponse(res, 200, `get ${this.nameDao} by email`, orders)
+        : this.handleResponse(res, 404, `${this.nameDao} by email not found`)
+    } catch (error) {
+      this.handleResponse(res, 500, error.message)
+    }
+  }
 }
 
 module.exports = OrdersController
