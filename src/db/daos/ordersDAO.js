9ea5709481a5e2cdd06e0f430c93173b5848ee31
EfimeroM <martinperez9713@gmail.com>
2023-08-10 16:42:31 -0300

    feat: add method getByEmail in orders

--- src/db/daos/ordersDAO.js
+++ src/db/daos/ordersDAO.js
@@ -5,6 +5,14 @@ class OrdersDAO extends BaseModelDAO {
   constructor () {
     super(orderSchema)
   }
+
+  async getByEmail (email) {
+    try {
+      return await this.schema.find({ 'user.email': email })
+    } catch (error) {
+      throw new Error(`Error getting ${this.nameSchema} by email - ${error.message}`)
+    }
+  }
 }
 
 module.exports = OrdersDAO
