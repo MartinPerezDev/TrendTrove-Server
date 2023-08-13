d929ca6f7afe9c9c85fce5388b9fe4b78a04c090
EfimeroM <martinperez9713@gmail.com>
2023-08-13 14:44:23 -0300

    feat: add route user/whishlist method post

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -4,10 +4,11 @@ const UsersController = require('../controllers/users.controller')
 const { authMiddleware } = require('../middlewares/auth.middleware')
 
 const usersController = new UsersController()
-const { signup, login, getUserByToken } = usersController
+const { signup, login, getUserByToken, addProductInWishList } = usersController
 
 usersRouter.post('/signup', signup)
 usersRouter.post('/login', login)
 usersRouter.get('/', authMiddleware, getUserByToken)
+usersRouter.post('/wishlist', authMiddleware, addProductInWishList)
 
 module.exports = usersRouter
