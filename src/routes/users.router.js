0c8a0f03a91167484ec2ddd4261b3d0a8ccbfc77
EfimeroM <martinperez9713@gmail.com>
2023-08-21 21:51:15 -0300

    feat: add route get api/user/wishlist/

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -4,11 +4,12 @@ const UsersController = require('../controllers/users.controller')
 const { authMiddleware } = require('../middlewares/auth.middleware')
 
 const usersController = new UsersController()
-const { signup, login, getUserByToken, addProductInWishList } = usersController
+const { signup, login, getUserByToken, addProductInWishList, getWishListById } = usersController
 
 usersRouter.post('/signup', signup)
 usersRouter.post('/login', login)
 usersRouter.get('/', authMiddleware, getUserByToken)
 usersRouter.post('/wishlist', authMiddleware, addProductInWishList)
+usersRouter.get('/wishlist', authMiddleware, getWishListById)
 
 module.exports = usersRouter
