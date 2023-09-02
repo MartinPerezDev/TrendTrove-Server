9062c23569f73ed7bee7eeda0cd8ec2fbf44c7d8
EfimeroM <martinperez9713@gmail.com>
2023-09-02 12:35:15 -0300

    feat: add route /wishlist with put method - DeleteProductInWishList

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -4,12 +4,13 @@ const UsersController = require('../controllers/users.controller')
 const { authMiddleware } = require('../middlewares/auth.middleware')
 
 const usersController = new UsersController()
-const { signup, login, getUserByToken, addProductInWishList, getWishListById } = usersController
+const { signup, login, getUserByToken, addProductInWishList, deleteProductInWishList, getWishListById } = usersController
 
 usersRouter.post('/signup', signup)
 usersRouter.post('/login', login)
 usersRouter.get('/', authMiddleware, getUserByToken)
 usersRouter.post('/wishlist', authMiddleware, addProductInWishList)
+usersRouter.put('/wishlist', authMiddleware, deleteProductInWishList)
 usersRouter.get('/wishlist', authMiddleware, getWishListById)
 
 module.exports = usersRouter
